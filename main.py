2a6a38d309dcac7acbe9aa02667baf06ea68312e
Maria Luque <maria.luque@elliptic.co>
2023-08-05 19:51:11 +0100

    change port

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session
 from flask_bootstrap import Bootstrap
 import random
+import os
 
 app = Flask(__name__)
 app.secret_key = "my-super-secret-key"
@@ -120,4 +121,5 @@ def getRandomCharade(selected_categories):
     return chosen
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
